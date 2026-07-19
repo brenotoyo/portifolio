cf8c0c9e04f4ccf23eb8acd8aa408388cfce1073
Breno Kazuo <breno_toyo@hotmail.com>
2026-07-19 14:59:48 -0300

    Ajustando erro de responsividade mobile, adicionando viewport no metadata de layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Portfólio - Breno Kazuo',
   description: 'Portfólio de Breno Kazuo',
+  viewport: 'width=device-width, initial-scale=1',
 }
 
 export default function RootLayout({
